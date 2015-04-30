553a8e29cbd6fbcc89e846073a6add4e04225269
Torben Knerr <mail@tknerr.de>
2015-04-30 07:35:39 +0200

    loop over the existing platforms and versions

--- spec/baseimage_spec.rb
+++ spec/baseimage_spec.rb
@@ -2,22 +2,30 @@ require 'spec_helper'
 
 describe 'vagrant-friendly docker baseimages' do
 
-  describe 'ubuntu-12.04' do
+  PLATFORMS = {
+    ubuntu: ["12.04", "14.04"]
+  }
 
-    around(:each) do |example|
-      Dir.mktmpdir do |dir|
-        @tempdir = dir
-        write_vagrantfile_with_provisioner(@tempdir, "ubuntu", "12.04")
-        example.run
+  PLATFORMS.each_pair do |platform, versions|
+    versions.each do |version|
+
+      describe "#{platform}-#{version}" do
+        around(:each) do |example|
+          Dir.mktmpdir do |dir|
+            @tempdir = dir
+            write_vagrantfile_with_provisioner(@tempdir, platform, version)
+            example.run
+          end
+        end
+        it 'is not created when I run `vagrant status`' do
+          cmd = Mixlib::ShellOut.new("vagrant status", :cwd => @tempdir)
+          result = cmd.run_command
+          expect(result.stdout).to include "not created (docker)"
+          expect(result.stderr).to match ""
+          expect(result.status.exitstatus).to eq 0
+        end
       end
-    end
 
-    it 'is not created when I run `vagrant status`' do
-      cmd = Mixlib::ShellOut.new("vagrant status", :cwd => @tempdir)
-      result = cmd.run_command
-      expect(result.stdout).to include "not created (docker)"
-      expect(result.stderr).to match ""
-      expect(result.status.exitstatus).to eq 0
     end
   end
 end
