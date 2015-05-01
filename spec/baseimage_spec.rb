2c19abbade968d86ed96b31111ed135b5914ce77
Torben Knerr <mail@tknerr.de>
2015-05-01 23:00:10 +0200

    run all tests in a single temp directory

--- spec/baseimage_spec.rb
+++ spec/baseimage_spec.rb
@@ -9,14 +9,16 @@ describe 'vagrant-friendly docker baseimages' do
   PLATFORMS.each_pair do |platform, versions|
     versions.each do |version|
 
+      before(:all) do
+        @tempdir = Dir.mktmpdir
+        write_vagrantfile_with_provisioner(@tempdir, platform, version)
+      end
+
+      after(:all) do
+        FileUtils.rm_rf @tempdir
+      end
+
       describe "#{platform}-#{version}" do
-        around(:each) do |example|
-          Dir.mktmpdir do |dir|
-            @tempdir = dir
-            write_vagrantfile_with_provisioner(@tempdir, platform, version)
-            example.run
-          end
-        end
         it 'is not created when I run `vagrant status`' do
           cmd = Mixlib::ShellOut.new("vagrant status", :cwd => @tempdir)
           result = cmd.run_command
