99027d9a128acabc1cd31ef07fe3dc13f30589d0
Torben Knerr <mail@tknerr.de>
2018-08-20 00:32:48 +0200

    extract function for running commands via mixlib / shellout so that we can debug more easily

--- spec/baseimage_spec.rb
+++ spec/baseimage_spec.rb
@@ -20,51 +20,44 @@ describe 'vagrant-friendly docker baseimages' do
 
       describe "#{platform}-#{version}" do
         it 'is not created when I run `vagrant status`' do
-          cmd = Mixlib::ShellOut.new("vagrant status", :cwd => @tempdir)
-          result = cmd.run_command
+          result = run_command("vagrant status", :cwd => @tempdir)
           expect(result.stdout).to include "not created (docker)"
           expect(result.stderr).to match ""
           expect(result.status.exitstatus).to eq 0
         end
         it 'comes up when I run `vagrant up --no-provision`' do
-          cmd = Mixlib::ShellOut.new("vagrant up --no-provision", :cwd => @tempdir)
-          result = cmd.run_command
+          result = run_command("vagrant up --no-provision", :cwd => @tempdir)
           expect(result.stdout).to include "==> default: Machine booted and ready!"
           expect(result.stderr).to match ""
           expect(result.status.exitstatus).to eq 0
         end
         it 'is now shown as running when I run `vagrant status` again' do
-          cmd = Mixlib::ShellOut.new("vagrant status", :cwd => @tempdir)
-          result = cmd.run_command
+          result = run_command("vagrant status", :cwd => @tempdir)
           expect(result.stdout).to include "running (docker)"
           expect(result.stderr).to match ""
           expect(result.status.exitstatus).to eq 0
         end
         it 'accepts remote ssh commands via `vagrant ssh -c`' do
-          cmd = Mixlib::ShellOut.new("vagrant ssh -c pwd", :cwd => @tempdir)
-          result = cmd.run_command
+          result = run_command("vagrant ssh -c pwd", :cwd => @tempdir)
           expect(result.stdout).to include "/home/vagrant"
           expect(result.stderr).to match ""
           expect(result.status.exitstatus).to eq 0
         end
         it 'can be provisioned with a shell script via `vagrant provision`' do
-          cmd = Mixlib::ShellOut.new("vagrant provision", :cwd => @tempdir)
-          result = cmd.run_command
+          result = run_command("vagrant provision", :cwd => @tempdir)
           expect(result.stdout).to include "==> default: Running provisioner: shell..."
           expect(result.stdout).to include "==> default: hello docker!"
           expect(result.stderr).to match ""
           expect(result.status.exitstatus).to eq 0
         end
         it 'can be stopped via `vagrant halt`' do
-          cmd = Mixlib::ShellOut.new("vagrant halt", :cwd => @tempdir)
-          result = cmd.run_command
+          result = run_command("vagrant halt", :cwd => @tempdir)
           expect(result.stdout).to include "==> default: Stopping container..."
           expect(result.stderr).to match ""
           expect(result.status.exitstatus).to eq 0
         end
         it 'can be destroyed via `vagrant destroy`' do
-          cmd = Mixlib::ShellOut.new("vagrant destroy -f", :cwd => @tempdir)
-          result = cmd.run_command
+          result = run_command("vagrant destroy -f", :cwd => @tempdir)
           expect(result.stdout).to include "==> default: Deleting the container..."
           # destroying containers does not work on circleci
           unless ENV['CIRCLECI']
